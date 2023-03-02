f4d72817c6cc85d5c602a5a3d7b9ab6009709372
Bao <online988988@gmail.com>
2023-03-02 09:54:31 +0800

    edit

--- quasar.config.js
+++ quasar.config.js
@@ -67,7 +67,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      publicPath: '/quasar-vacation',
+      publicPath: '/',
       // analyze: true,
       env: {
         VITE_API: process.env.VITE_API
